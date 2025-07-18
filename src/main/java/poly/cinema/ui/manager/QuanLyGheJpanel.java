180a4ccee1f169bc211e65e29236d1e32526a5ae
klynh1222 <vothuykhanhlinh@gmail.com>
2025-07-18 22:18:03 +0700

    bỏ boolean

--- src/main/java/poly/cinema/ui/manager/QuanLyGheJpanel.java
+++ src/main/java/poly/cinema/ui/manager/QuanLyGheJpanel.java
@@ -253,19 +253,12 @@ public class QuanLyGheJpanel extends javax.swing.JPanel
 
             },
             new String [] {
-                "Mã ghế", "Số ghế", "Hàng", "Cột", "Loại ghế", "Mã phòng", ""
+                "Mã ghế", "Số ghế", "Hàng", "Cột", "Loại ghế", "Mã phòng"
             }
         ) {
-            Class[] types = new Class [] {
-                java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class
-            };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, true
+                false, false, false, false, false, false
             };
-
-            public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
-            }
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
